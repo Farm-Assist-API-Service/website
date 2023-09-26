e562c52eedfe6a5201a26ede22850d3650f759e2
God'sgift Uko <godsgiftuko@gmail.com>
2023-09-26 01:33:37 +0100

    added app-release download link

--- src/components/sections/download.vue
+++ src/components/sections/download.vue
@@ -5,10 +5,10 @@
         <div class="bg-fixed bg-contain lg:bg-auto bg-download p-8 sm:p-16 md:px-24 md:py-24 lg:px-32 lg:py-20 bg-no-repeat bg-bottom rounded-[40px] md:rounded-[100px] min-h-200">
             <h1 class="font-medium text-white text-2xl md:text-5xl text-center">Download Farm Assist on App store and Play store</h1>
             <div class="flex justify-center gap-5 mt-10 md:mt-20 mx-0 mb-0">
-                <a href="/" class="text-brand-dark no-underline hover:underline">
+                <a href="#" class="text-brand-dark no-underline hover:underline">
                     <img src="/src/assets/images/pngs/download/app-store.png" alt="Download Farm Assist on App store" class="w-52 h-auto xs:h-14 sm:h-16"/>
                 </a>
-                <a href="/" class="text-brand-dark no-underline hover:underline">
+                <a href="https://doc-10-08-docs.googleusercontent.com/docs/securesc/ks8htauec257c84oooubl84lkhn4lu5e/go86a3v0h5au18upl56st90kvc4ndf4m/1695688200000/09107763162123321931/10863867453056133873/1-7ZK79KH4FSmJBj3umfmF6o4fXp5Fc_Y?e=download&ax=AA75yW4lPiTFML7s8xoaPiMgsQgYZOqtagSDc25HMxmBclOb8aNtGLa0vV29kjhcVHLVvcmfQbO4j9F0ReKmuw896qBA6_IQP33nsNd7knRiYisc-SXT_4mvjXOgUErBC6YNYncGkOfzGitLzjvSNGevoX150boVkQuVO-ookypSI3hq_pU1fw-uKE1T96fHLi9_SCByFwXzH2rXwbvMbPl7yuvTSff_OTQVxx80pll1urREOPm2m4zBsbNYulpRZbrTchSDUufdDd0Uc-569SyR2tGdu4kUOGLix0G9OQtTdDUUr-tlQygE-XwCUU639kA7IqCjH0dkg8Oi_S32NLIgCgD7SOvJe2NKPVBKXBxAq1NBenhb7TRExYUKOhWI3XqsxAAcZkuCVfUJ2lbZ5oFRZBxdpq8uNy72b9Gul7Vxl5waRufP0ZVR3Jryo7TsKfHWb6QBr_YqGBvsi5Fb5yczZYM2_7xTtPBpKa6nBedFHVMNFO7kNuZRTUeDHVi4jgrj7bF_dcbAjRATpXfeZQN9IwaetfiCYW2GUabIsOQMOtCTKquW8eh-BkRXKfC7yjHX6w1hIST127a4jxX_JhOijvWF7dtZ9L1UhIvV69dojSdwU3VGUw7iNg6fVeAZJ2p4II3UVLTP2mJrRyfEreAIjXs3Lq7RVAgd9TeTXLMsEldzmEdQWSVfwhLuaWnUbM72hpUG7-k6BwZKsjuzvBJj36IUrVVh8xbw8y8IFSg9y36moGl3IGaiXhOU2xb7kvFKFEJVwdTYRX6KBJjXDe3h159I3GgmrTFsQHtAG5Cdm80ONerJcCc58qSOaJrh5f0p56hxfAn2cWfhr-Gkp4Zr1vUKU8jxUHqxZanl7_JYq8Blmp3UC8nMlG2KEPM0qVTczT3anCDEn5bBwvxmzNHba0FE3mcwxH_ZhREHHqT-_-3Mw4cn0cLewuihwTd0Pl5IrSIhrXeLgIy4LFFVzdjGOPvVWLf4LLztvZNQNVj_xhY&uuid=28fd8128-25cb-41ba-9a70-bf5edf2da481&authuser=0&nonce=dcbmtbj3n6hac&user=10863867453056133873&hash=ak7laeup8910n1f1jhfsbm38f5rjfm9uß" class="text-brand-dark no-underline hover:underline">
                     <img src="/src/assets/images/pngs/download/play-store.png" alt="Download Farm Assist on Play store" class="w-52 h-auto xs:h-14 sm:h-16"/>
                 </a>
             </div>
